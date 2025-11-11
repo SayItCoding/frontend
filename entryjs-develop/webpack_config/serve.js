9e5d5731b1a89f11a072d9f783c2588acad96c59
June8039 <yeonlee8039@gmail.com>
2025-11-11 11:13:52 +0900

    configured files so it runs on the PC, might not work in other environments

--- entryjs-develop/webpack_config/serve.js
+++ entryjs-develop/webpack_config/serve.js
@@ -32,7 +32,7 @@ module.exports = {
     ],
     devServer: {
         static: {
-            directory: path.join(__dirname, '../'),
+            directory: path.join(__dirname, '../dist'), // changed it from ../ -> ../dist
         },
         port: devServerPort,
         historyApiFallback: true,
