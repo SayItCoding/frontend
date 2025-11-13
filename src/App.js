3cb30651c72893496fd29db20d38a3d1790acac7
June8039 <yeonlee8039@gmail.com>
2025-11-13 18:45:13 +0900

    Add: 소리 관련 처리를 위한 soundSetup.js 만듬. 현재는 디버깅 목표

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
-import React from "react";
+//import React from "react";
+// App.js
+import React, { useEffect } from "react"; // ← useEffect 반드시 import
+
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import EntryMission from "./pages/EntryMission.jsx";
 import LoginPage from "./pages/LoginPage.jsx";
 import SignupPage from "./pages/SignupPage.jsx";
+
+import { initSound } from "./soundSetup";
 
 function Home() {
   return (
@@ -16,6 +21,12 @@ function Home() {
 }
 
 export default function App() {
+
+  //여기서 soundSetup사용하는데 필요 없으면 지워도 됨
+  useEffect(() => {
+    initSound(); // 앱 로드 시 SoundJS 초기화
+  }, []);
+
   return (
     <Router>
       <Routes>
